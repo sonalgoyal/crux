6db4e62d0296710dd94ccc7fde3741a7c509607e
Alex Newman <posix4e@gmail.com>
2011-09-16 14:52:12 -0700

    BUILD Cleanup of crux 	Moved to a pom project 	It is now self runnable

---
+++ src/test/java/co/nubetech/crux/dao/DBConnection.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2011 Nube Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package co.nubetech.crux.dao;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnection {
+	private java.sql.Connection conn;
+	private Statement stmt;
+
+	public Statement getStatement() {
+
+		String userName = "root";
+		String password = "";
+		String url = "jdbc:mysql://localhost/crux";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+			conn = DriverManager.getConnection(url, userName, password);
+			stmt = conn.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return stmt;
+
+	}
+
+	public void closeConnection() throws SQLException {
+		stmt.close();
+		conn.close();
+	}
+
+}
